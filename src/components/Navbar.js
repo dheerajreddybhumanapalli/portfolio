ac00ef3ea93b6ec9b75ac471bf320ad001f9a364
Dheeraj Bhumanapalli <dbhumanapalli@comply.com>
2025-05-03 09:45:48 +0530

    addedd_navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import '../styles/Navbar.css'
+
+export function Navbar(){
+    return (
+        <div className="navbar">
+            <div className="navbar-right">
+                <button>Home</button>
+                <button>Experience</button>
+                <button>Resume</button>
+                <button>Contact</button>
+            </div>
+        </div>
+
+    )
+}
