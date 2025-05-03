39f1de343ccb723098e060b89d16d5acd1c7b68f
Dheeraj Bhumanapalli <dbhumanapalli@comply.com>
2025-05-03 14:34:33 +0530

    changed_icon_and_navbar_css_added_contact_page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,11 @@
+import '../styles/Experience_Contact.css'
+
 export default function Contact(){
     return (
-        <h1>Contact</h1>
+        <div className="base">
+        <h1>Contact me at</h1>
+        <p>Email : <a href="mailto:dheerajbhumanapalli.com" style={{ color: "rgb(52, 139, 239)" }}>dheerajbhumanapalli@gmail.com</a></p>
+        <p>Mobile : (+91)7674051671</p>
+        </div>
     )
 }
