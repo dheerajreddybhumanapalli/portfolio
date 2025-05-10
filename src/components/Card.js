66589d9836105095ac223a8a3a5677fe1d8e1205
Dheeraj Bhumanapalli <dbhumanapalli@comply.com>
2025-05-10 18:59:30 +0530

    added card components with style change

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,26 @@
-export default function Card({ title, designation, duration, description}){
+import { useState } from "react"
+import '../styles/Card.css'
+
+export default function Card({ title, roles}){
+    const [open, setOpen] = useState(false)
     return (
-        <div className="card">
+        <div className="card" onClick={() => setOpen(!open)}>
             <h2>{ title }</h2>
-            <div style={{ margin: '25px'}}>
-                <h3>{ designation }</h3>
-                <h4>{ duration }</h4>
-                <ul>
-                    {description.map((point, index) => (
-                    <li key={index}>{point}</li>
-                    ))}
-                </ul>
-            </div>
+            {open &&
+            <ul>
+            {roles.map((role, role_index) => (
+                <div key={role_index} style={{ margin: '25px'}}>
+                    <h3>{ role.designation }</h3>
+                    <h4>{ role.duration }</h4>
+                    <ul>
+                        {role.description.map((point, description_index) => (
+                        <li key={description_index}>{point}</li>
+                        ))}
+                    </ul>
+                </div>
+            ))}
+            </ul>
+            }
         </div>
     )
 }
